8ae1cd6af8ab16aa405e3186950d2cb0d234d2d6
Ubuntu <ubuntu@ip-172-31-44-148.ap-south-1.compute.internal>
2026-04-06 18:00:25 +0000

    Test webhook automation

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello from Docker + Jenkins CI/CD Pipeline 🚀"
+    return "CI/CD Pipeline is fully automated 🚀🔥"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3000)
